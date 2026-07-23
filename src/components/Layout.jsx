f8c973e58d4090de271638d82e8bc41b97ea37a6
masoume78 <masoumehmasouminia@gmail.com>
2026-07-23 16:51:11 +0330

    add developer name to footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,7 +10,31 @@ function Layout() {
         <Outlet />
       </main>
       <footer className="app-footer">
-        <p>Powered by OpenWeatherMap</p>
+        <p>
+          Powered by
+          <a
+            href="https://openweathermap.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-semibold text-blue-500 hover:underline"
+          >
+            OpenWeatherMap
+          </a>
+        </p>
+        <p>
+          Designed & Developed with
+          <Heart size={14} className="inline text-red-500" /> by{" "}
+          <span className="font-bold text-gray-800 dark:text-white">
+            <a
+              href="https://www.linkedin.com/in/masoume-masoumi/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-semibold text-blue-500 hover:underline"
+            >
+              Masoume Masoumi
+            </a>
+          </span>
+        </p>
       </footer>
     </div>
   )
