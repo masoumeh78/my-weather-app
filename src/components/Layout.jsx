75bc6ab31862b061b0a6e32ab342acab52b57c5a
masoume78 <masoumehmasouminia@gmail.com>
2026-07-23 17:06:21 +0330

    fix bugs in run app

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
-import { memo } from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar.jsx'
+import { memo } from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar.jsx";
 
 function Layout() {
   return (
@@ -18,26 +18,22 @@ function Layout() {
             rel="noopener noreferrer"
             className="font-semibold text-blue-500 hover:underline"
           >
-            OpenWeatherMap
+            {" "}
+            OpenWeatherMap{" "}
           </a>
-        </p>
-        <p>
-          Designed & Developed with
-          <Heart size={14} className="inline text-red-500" /> by{" "}
-          <span className="font-bold text-gray-800 dark:text-white">
-            <a
-              href="https://www.linkedin.com/in/masoume-masoumi/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-semibold text-blue-500 hover:underline"
-            >
-              Masoume Masoumi
-            </a>
-          </span>
+          | Designed & Developed with 💖 by{" "}
+          <a
+            href="https://www.linkedin.com/in/masoume-masoumi/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-semibold text-blue-500 hover:underline"
+          >
+            Masoume Masoumi
+          </a>
         </p>
       </footer>
     </div>
-  )
+  );
 }
 
-export default memo(Layout)
+export default memo(Layout);
