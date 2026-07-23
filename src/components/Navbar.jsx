17fb1608e2299013cce2966e047188c3c0eac373
masoume78 <masoumehmasouminia@gmail.com>
2026-07-23 18:08:13 +0330

    change title in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <header className="navbar">
       <NavLink to="/" className="navbar__brand">
-        <span aria-hidden="true">☁</span> Sky Cast
+        <span aria-hidden="true">☁</span> SkyCast
       </NavLink>
       <nav className="navbar__links" aria-label="Main navigation">
         <NavLink to="/" end>
